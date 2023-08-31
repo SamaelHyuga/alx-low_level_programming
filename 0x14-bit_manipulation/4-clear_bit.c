31c6058c5ff72272a190da252784d5ff6ac0d515
SamaelHyuga <lifa4real@gmail.com>
2023-08-31 03:24:47 -0700

    Bit manipulation ssj

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * clear_bit - sets value of given bit to 0
+ * @n: pointer to number to change
+ * @index: index of the bit to clear
+ *ssj
+ * Return: 1 for success, -1 for failure
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 63)
+		return (-1);
+
+	*n = (~(1UL << index) & *n);
+	return (1);
+}
